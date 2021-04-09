638f11276b4ce1ad819dbcbb97c55b37d2e20007
mobdevkyel <79609322+mobdevkyel@users.noreply.github.com>
2021-04-09 11:32:51 -0300

    Add files via upload

--- busca.py
+++ busca.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#   OfusPy 2.0.5b6 - Ofuscador Python
+#   Pacheco, Matias W. <mwpacheco@outlook.es>
+#   Copyright (c) 2021 Wehaa Portal Soft.
+#   License MIT
+#
+#   WARNING! Do 'NOT' change. You may experience problems with the file.!
+#
+################################################################################
 
 import zlib, base64
 exec(zlib.decompress(base64.b64decode('eNrdk1FL3EAQx98D+Q7hRJKDJTFXi63QB0Ww92BbUOiDSBmTvbstm+y6O1uln97Zy24SKYLgWw+ScL+Z+c9/ZpM0EZ1WBjPDHxy3aNNkY1SX3dvjLETOOTgUGyevldNpzP9tVZ8madLyTdYKq1Uv/nBhi+VpmmT0c0ZmX7J8h6jtaVU1gCDVFlplyk5WoEVFMg3vEba8uwTJV9VVnXtFX91IsPaXFBZJxHIslgPXlE0kmi23FKJOIWrJIEVfGC58SdmoHqkX84Y6WWowlpt8GduhapWlSj9UKRW09kVZkL9YX//4Tln77NucRLjN74aY4ehMP6TEtcSZzbiU+f0/W1Bsb47qaUU3/pHf3R6FLQG6Kegkig7O0IEUf6EBFXep+X5uytzLjdUH9HeE9QRbA2rkq0lkZB8CQ3gCvy40BYVJ4g0HO7vPAeWIFlr+j5/X7Exn6RuRkdJwLaHhxeJwwbLFInj5uf7mi8neSSi9rAP4HMEqgDpu5uv6JqBPgTyKPpDoYBtljiOIMh8D2AkMxC9sYFdc7lTY5H5gFiZk85GYt83IKV0r5u0w74BRU7qIkHQ8LiN6LAbZ8bUJX8+7ezD/hqXJwezLS5NnsCpluw==')))
--- melhor_payout_par.py
+++ melhor_payout_par.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#   OfusPy 2.0.5b6 - Ofuscador Python
+#   Pacheco, Matias W. <mwpacheco@outlook.es>
+#   Copyright (c) 2021 Wehaa Portal Soft.
+#   License MIT
+#
+#   WARNING! Do 'NOT' change. You may experience problems with the file.!
+#
+################################################################################
 
 import zlib, base64
 exec(zlib.decompress(base64.b64decode('eNqtVsuumzAQ3fMVo1RVTC+lSZeR0mWlblpVvTuEkBNMYgUwtZ3HVdV/7/gBgZDHvVKzAnvmnHmdIbxqhNSgecUiqKjeBoUUFfDfotFc1LThsdJ0VbIMH4E7628/sx/2OgiCnBXAVUaVYjoTDauJfYyAlqV9Z7m7VIgvcrac6r1ciWm4CAB/kuFrPTZOjG2a2Jc0mZi7SerYNkg0sicYsQlxCOsP475LZnIl4RCrkaLgr0Vxxn2InG+4pt2Nd8AarDU/YGHZqeGSmop55BZS7VdqLfmKZUpLvmNZyZUmV9ysV0NfxF7DchBRy73eS8lq3cZwC+O45SWDWmiP5uIxP1OXWJWMNWQWz8Pu/H+wtq77+m0Z+w78mXq+6cIOaVyUQkjiIgvhE8xns7+uHZVQbTSZmaFzMxrKZZ8B55HXmcdwZciqpsJEk44ugngWwVdaKobTZ0xGk4f29yfSurkrtM352oyOOSuEtJIAXiOlk0UEHXd6bo3zdpowjRjozeV1Q26+/EUL4WlSQO726Aqh10Mvt0uFdKY97CtA3sLEaNL0KMP7HmFiDNN43+RUM3JPWpfdDMMBJsOOPWDxyb2aoFccLiTXL3a+7PyamUm7e9PXXQQHk7ArTcw1qxQJR3U5XIvxEj2mDaLkJEHQQQbJLk0fhKVwX7OcjO8i2LGXZUmrVU7htIBTMk8j1NuBScWWz3LPBi0eAwwjN8cRrJgRX7svxj7JLB14vUOTWpOR87CXZ6Pwsn59wi/LnqDHdeWFE9tyCV4EY5t2B2CckXuYtw+fzbx0Iu3xRmCKNYK6PoAPCc6L5z7FdXhfT9ysxAjIbvRaHEkYRjB53rJ2l5sJncCTk+UTPnKFfKU42lPjjguWatJvCXwwWzY05u8nYfCmrBzEZVJ2rQa3M7qXjVPn5CP8EhXTW15v4IjfIThKUW9i+C7ASs5IEMlkN4tgdbEgFxncjZ5JKeTt4IeBPwq6Lfe6FAo/CHgVD8O5G4rzuh5L/4N5EwT/sf0DJMEgjg==')))
