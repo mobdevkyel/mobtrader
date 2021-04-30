d8ba318ba818eee55012d9cfb1cd7233f8d6d1d4
mobdevkyel <79609322+mobdevkyel@users.noreply.github.com>
2021-04-30 11:36:00 -0300

    Add files via upload

--- busca.py
+++ busca.py
@@ -1,4 +1,15 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#   OfusPy 2.0.5b6 - Ofuscador Python
+#   Pacheco, Matias W. <mwpacheco@outlook.es>
+#   Copyright (c) 2021 Wehaa Portal Soft.
+#   License MIT
+#
+#   WARNING! Do 'NOT' change. You may experience problems with the file.!
+#
+################################################################################
 
 import zlib, base64
-exec(zlib.decompress(base64.b64decode('eNrNU8Fq3DAQvRv8D2ZDsBeEHW8T2gZ6aAls9rBpIYEcQihTW7urIluKNGpLQv69o7VkbyiFll5qkI3fzLx58ySliei0MpgZ/uC4RZsmG6O67Is9zULkAweHYuPktXI6jflfrerTJE1avslaYbXqxTcubDE/T5OMHmdk9i7Ld4janldVAwhSbaFVpuxkBVpURNPwHmHLuyVIvqjWde4ZfXUjwdrPUlgkEsuxmA+4pmxCothySyHqFKKWBFL0heDCl5SN6pF6MS+ok6UGY7nJ57EdqlZZqvRDlVJBa1+UBfqL1fWnj5S1z77LiYTb/H6IGY7O9ENKtCXObIo1W0ZfDt+DR5u/Mulp/fz/uhTbm5N68unGf/L7u5NgFaCbgk6i6OA9OpDiERpQ0VDN94NT5p5urD6i3xGsJ7A1oEZ8MZGM2KuAIfwAbxeagsJE8Qe7e/A+BChHtNDyX/T8Ts60mb4RCSkN1xIaXsyOZyybzYKW29WVLyZ5r0Ppsg7A2wgsAlBHZy5XNwF6E5Dvog9IVLCNNKcRiDRnAdgJDIg3bMDWXO5UcHI/MAsTssORmJfNSCmtBfNymFfAqCktQog6bpcRPRYD7XhswhX65x7Mn7A0OZquH7X4CXjbZ6g=')))
+exec(zlib.decompress(base64.b64decode('eNrNVFFr2zAQfjf4P4iMYQeEHXsd3Qp56MjI8pC10EIfShk3W3Y0ZMuT5G209L/vFEuOQ+lW2MsMtvF393336e6SMOBNJ5Uhin3vmTY6DColG/JVnxAX+cCgN7zqxZXsu9Dnf9OyDYMwKFlFSq472fIfjOt4S9fzszAgePVKkCWJdsZ0+ixNCzAgZA2lVEkjUuh4ikoFaw3UrFmDYHm6zSIratmFAK2/CK4Nimhm4vmAd5iNiPeb1BjCSi6q0SNGjzzHlpIUsjVYi1pDjUg6UJqpaO7LGVlKjUx7rkRIKPURzcmvNleXF5i1z76NUITp6G6IKWZ61Q4pvjP+zMr2hW4vPq7OfXd4RdZkuSQL922vGptgTUSTxtzwdsUuFW8YVxANuUx4dvYXtm1r9oSVv4CVO9b0OUy0+tNIH/Z6j+nD9nEUOH7/d4P15dUiO4z22r6iu9uFmy6Y/hDsheENnJseBL+HAqTfgY7te4iZe7mR/Qo/RzA7gKUCOeL5QWTE3jjMwC+w7TIqxjBKvGAhJ88pgDm8hJI98fOcncNe2EJoJFGsE1CwePZ6Rsls5rzcbD5bMto7ddR15oD3HsgdkPnOfNpcO+idQ37y1iHeQe1lTjzgZd46YMeNQ2zDBmzLxE66Tu4PTN0J6fRI1Nqm6BTvnFo71DqgWBRvRFDaj0vx1sSD7Lg27lf/zzWo3bAwcEWm/6hzrPUbcQ6X5g==')))
