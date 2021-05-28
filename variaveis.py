6bfccf1877eb9232823aafc8c7f9b0f35eab0bce
mobdevkyel <79609322+mobdevkyel@users.noreply.github.com>
2021-05-28 11:30:37 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#______  ___       ______  ________                _________#             
+#___   |/  /______ ___  /_ ___  __/______________ _______  /_____ ________#
+#__  /|_/ / _  __ \__  __ \__  /   __  ___/_  __ `/_  __  / _  _ \__  ___/#
+#_  /  / /  / /_/ /_  /_/ /_  /    _  /    / /_/ / / /_/ /  /  __/_  /# 
+#/_/  /_/   \____/ /_.___/ /_/     /_/     \__,_/  \__,_/   \___/ /_/# 
+#FULL MHI V 1.0 - Contato: +55 (11) 9 7615-9233#
+
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eNptVkuP2zYQPleA/oOAHtIARbH2NrntwevVOkJlr7pWNi2KQphItM2CErUk5cT59R3xIVFa2wcNvxnO8xvZB8HrqOSMC6ghonXLhYoeuSC/Rnt1YSQMwuDnO/2JzkRI4NGd/YQBAtFdtPg4mIyfaJM+3a+SfTSFwyATRKEPHeO3+3S1/iMMXoioCTsN8HP8oMGKOGTzHMe7MFjVIAgb7P6O0/TpC8I/Oja6/ByHwRaOpFGA4Dvn8V0YrC/QzCHa/IBizUDwq5pYlt0blcvX3ZvpKnJNYXN3qp8mOizg2h1bxvX8sJirih05CqpmnpIGh6XIzPSZSKIQ06NGbB/nxSpN+6Gna8RvwuAIzYlLlA+Mg/rl5n0YtESUMIVAdcAmSMlt/zHMV912zwEIWoGebq9uoRLA3YGLsq/5SGqTwJdkZ4TNwj6X5vkpyY3wjTY2V2txtBYnqoywtc55Ww6BXjtolEtjzA2npGhzBEbM1cd0oj4v7fXzjRN+t0J523MQmMSluYemhII2tKSzvjT8zEe7fZ75h6csfdrvTeD+NNR+v9qtV8kuWSer1CDYcSOo1saXVCpSg0FbPaAeZvxoJfrKC2ipPb249j3Ez0Z4sc/d2TwFkTYG60oj6KYZUQKrXC9LUFaqaXOCgmEqfSL//Iu6E5zJzqr770tuK6z1LBbavdAc62dHBGmwa0bTCvJf52W18CwqGFt3wveX1+4+lIb+sgcikbS2Nat8vJdnmhrbxYd+C/PPQ5oNrx09JcGSnJ8aqAvgMxwZK0xC+obiLeNSekfD0f4EneLonXtpOsOedU4+gOLCUYwwkI7DoOgZRhpLLvjGu4fjHkIhCeQ3Lip3syx516hCXVpX22Q4FYa52OGiV2nmg6d4Z6mCcwYMABUXhV0RO/4BRgq2/ApeczL0ZzPkd7FLDiW+Uo1IxMCFtmNOtK64Y3f/xigO9DufvENyAV/B/JAYUHgmf+YPj/Q7WIe8LSpyQMoMWY3f7adkpAgelpPT7VRXbIFyQWFqcwWl7IQ5b2lzHZ/bZ6bGpMYqPMJygWtZbEhNdCUO7sEtl68dUYSaBlrV2s+3/9USWHhx+ybQclrX24So+QnxksG7/hJk0y3IZmuQTfcgc2zHYWSa4U5h2n9jpOUo3nqol57TzqF5v31wYjnrdA/N26yxKz3WPidN9RwuvYRnIUfmZx71M4/7mddufaNjLjvZQaEX1zask0QUtLKLwiiOwan8rVO0Jgf8m+d0arosebx7QDEXHfkfzivD9w==')))
